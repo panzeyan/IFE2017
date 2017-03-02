088403758a55d848bae316f8f58ce377c4a5bdd2
panzeyan <panzeyan@gmail.com>
2017-03-02 23:23:30 +0800

    parse quote

--- markdown文件解析器/js/parser.js
+++ markdown文件解析器/js/parser.js
@@ -23,15 +23,20 @@ var mdParser = {
     var count,
         stra = [];
     
-    // 处理标题
+    // 解析标题
     while ((stra = mdParser.regex.headline.exec(str)) !== null) {
       count = stra[1].length;
       str = str.replace(stra[0], '<h' + count + '>' + stra[2] + '</h' + count + '>');
     }
 
-    // 处理代码块
+    // 解析代码块
     while ((stra = mdParser.regex.codeblock.exec(str)) !== null) {
       str = str.replace(stra[0], '<pre><code>' + stra[1] + '</code></pre>')
+    }
+
+    // 解析引用
+    while ((stra = mdParser.regex.quote.exec(str)) !== null) {
+      str = str.replace(stra[0], '<blockquote><p>' + stra[1] + '</p></blockquote>')
     }
 
     return str;
