7217180dc58ea3356f3282f2b2b503a56d8e1e44
panzeyan <panzeyan@gmail.com>
2017-03-03 20:04:36 +0800

    改变触发解析markdown的条件

--- markdown文件解析器/js/main.js
+++ markdown文件解析器/js/main.js
@@ -1,10 +1,12 @@
 var editorInput = document.getElementById("editor-input");
 var previewer = document.getElementById("previewer")
 
-editorInput.addEventListener("keyup", function (e) {
+previewer.innerHTML = mdParser.parse(editorInput.value);
 
-  if (e.keyCode == 13) {
-    var value = e.target.value;
+editorInput.addEventListener("keyup", function (e) {
+  var value = e.target.value;
+
+  if (value.length > 0) {
     previewer.innerHTML = mdParser.parse(value);
   }
 }, false);
