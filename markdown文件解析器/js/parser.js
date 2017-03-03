5f838c62ad80f1ab55d95986ea540afccfc29a5a
panzeyan <panzeyan@gmail.com>
2017-03-03 19:57:16 +0800

    parse lists

--- markdown文件解析器/js/parser.js
+++ markdown文件解析器/js/parser.js
@@ -7,10 +7,10 @@ var mdParser = {
     headline: /^(\#{1,6})([^\#\n]+)$/m,
 
     // 无序列表
-    ul: /^\*\s+([^\n]+)/m,
+    ul: /^([\*|\-] ([^\n]+)\n)+/gm,
 
     // 有序列表
-    ol: /^\d\. ([^\n]+)/m,
+    ol: /^(\d+\. ([^\n]+)\n)+/gm,
 
     // 引用
     quote: /^>\s+([^\n]+)/m,
@@ -20,7 +20,7 @@ var mdParser = {
   },
 
   parse: function (str) {
-    var count,
+    var count, strlist, line,
         stra = [];
     
     // 解析标题
@@ -37,6 +37,39 @@ var mdParser = {
     // 解析引用
     while ((stra = mdParser.regex.quote.exec(str)) !== null) {
       str = str.replace(stra[0], '<blockquote><p>' + stra[1] + '</p></blockquote>')
+    }
+
+    // 解析列表
+    // 无序列表
+    while ((stra = mdParser.regex.ul.exec(str)) !== null) {
+      repstr = '<ul>';
+      strlist = stra[0].split('\n');
+      for (var index = 0; index < strlist.length; index++) {
+        var element = strlist[index];
+        if (element.length > 0) {
+          line = /^[\*|\-] ([^\n]+)/.exec(element);
+          repstr = repstr + '<li>' + line[1] + '</li>';
+        }
+      }
+      repstr = repstr + '</ul>';
+
+      str = str.replace(stra[0], repstr);
+    }
+
+    // 有序列表
+    while ((stra = mdParser.regex.ol.exec(str)) !== null) {
+      repstr = '<ol>';
+      strlist = stra[0].split('\n');
+      for (index = 0; index < strlist.length; index++) {
+        element = strlist[index];
+        if (element.length > 0) {
+          line = /^\d+\. ([^\n]+)/.exec(element);
+          repstr = repstr + '<li>' + line[1] + '</li>';
+        }
+      }
+      repstr = repstr + '</ol>';
+
+      str = str.replace(stra[0], repstr);
     }
 
     return str;
