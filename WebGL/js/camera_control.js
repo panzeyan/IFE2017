c82e185c88251b5ad332685d7904383ec118a4c8
panzeyan <panzeyan@gmail.com>
2017-03-08 18:03:02 +0800

    添加注释

--- WebGL/js/camera_control.js
+++ WebGL/js/camera_control.js
@@ -1,21 +1,24 @@
 var controls = new THREE.TrackballControls(camera);
 
-controls.rotateSpeed = 10.0;
-controls.zoomSpeed = 3.0;
-controls.panSpeed = 0.8;
+// 设置3种控制方式的速率
+controls.rotateSpeed = 10.0; // 旋转
+controls.zoomSpeed = 3.0; // 缩放
+controls.panSpeed = 0.8; // 平移
 
-controls.noZoom = false;
-controls.noPan = false;
-
-controls.staticMoving = true;
+// 控制照相机时带惯性
+controls.staticMoving = false;
 controls.dynamicDampingFactor = 0.3;
 
-// controls.keys = [65, 83, 68];
-
+// change事件发生时重新渲染画面
 controls.addEventListener('change', render);
 
 function animate() {
+
+  // https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+  // 向浏览器请求调用animate
   requestAnimationFrame(animate);
+
+  // 更新controls
   controls.update();
 }
 
